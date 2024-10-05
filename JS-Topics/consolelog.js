1899c5130260d0a3157aec832f5b8b7d371b7d5d
kumar8688 <kumarborelli0@gmail.com>
2024-10-05 21:53:54 +0530

    js-Topics3

--- JS-Topics/consolelog.js
+++ JS-Topics/consolelog.js
@@ -44,7 +44,16 @@ console.log(isNaN({})) //true
 console.log(isNaN([])) //flase
 console.log(isNaN([1,2,3])) //true
 console.log(isNaN(['apple','banana'])) //true
-
+//0 == false   // true
+//0 === false  // false
+//1 == "1"     // true
+//1 === "1"    // false
+//null == undefined // true
+//null === undefined // false
+//'0' == false // true
+//'0' === false // false
+//[]==[] or []===[] //false, refer different objects in memory
+//{}=={} or {}==={} //false, refer different objects in memory
 
 
 
