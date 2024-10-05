6dd9fa5acbf576779b26beecb40d5b96d7df090b
kumar63086 <128176566+kumar63086@users.noreply.github.com>
2024-10-05 22:15:09 +0530

    Create oops.js

---
+++ JS-Topics/oops.js
@@ -0,0 +1,29 @@
+javaScript allows Object-Oriented Programming (OOP) using classes and prototypes. Here are the key concepts:
+
+Classes and Objects:
+
+Classes are blueprints for creating objects with properties and methods.
+Objects are instances of classes.
+Encapsulation:
+
+Restricts direct access to object properties.
+Private properties can be created using # in ES6.
+Use getters/setters to access private properties.
+Inheritance:
+
+One class can inherit properties and methods from another class using the extends keyword.
+super() is used to call the parent class's constructor.
+Polymorphism:
+
+Subclasses can override methods of the parent class to provide specialized behavior.
+Abstraction:
+
+Hides complex details and exposes only necessary parts.
+Simulated using method overriding and inheritance in JavaScript.
+Static Methods:
+
+Methods that belong to the class itself, not instances of the class.
+Prototypes:
+
+Every object in JavaScript has a prototype from which it can inherit methods and properties.
+This combination of encapsulation, inheritance, polymorphism, and abstraction forms the basis of OOP in JavaScript.
