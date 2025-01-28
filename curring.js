237e0cdcd4cab1e29dda98e1a9cc6bda2459c1a3
kumar63086 <128176566+kumar63086@users.noreply.github.com>
2025-01-28 10:59:31 +0530

    Update curring.js

--- curring.js
+++ curring.js
@@ -20,3 +20,25 @@ function add (a){
 }
 
 console.log(add(2)(3)(6)()); 11
+const Arra = [1, 2, 3, 4, [3, 4, [6, 8], 9], 10];
+
+function fallted(arr){
+    return arr.reduce((acc,val)=>
+     acc.concat(Array.isArray(val)?fallted(val):val),[]
+    )
+}
+console.log(fallted(Arra))
+
+const words=['kumar','srinuvas','praveen']
+const longestwordslength=words.reduce((maxlength,word)=>{
+    let currentlength=word.length
+  return  currentlength > maxlength?currentlength:maxlength
+},0)
+
+console.log(longestwordslength)
+
+const longestword=words.reduce((longestword,word)=>{
+    return word.length>longestword.length?word:longestword
+})
+console.log(longestword)
+
